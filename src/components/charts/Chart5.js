f6998b860efd971b19b845d142872b3e0b443173
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-09-10 12:50:30 +0530

    minor-changes

--- src/components/charts/Chart5.js
+++ src/components/charts/Chart5.js
@@ -13,77 +13,77 @@ export default function Chart5({ year }) {
   const callFunction = async () => {
     const len = (await data.data) && data.data.length;
     if (len > 0 && dataLoaded === false) {
-      chart_5_data(year);
+      // chart_5_data(year);
       setDataLoaded(true);
     }
   };
   callFunction();
   useEffect(() => {
-    chart_5_data(year);
-  }, [year]);
-  const chart_5_data = (year) => {
-    let MostWins = {};
-    data.data &&
-      data.data.forEach((match, idx) => {
-        if (match.season === parseInt(year)) {
-          if (MostWins[match.winner] !== undefined) {
-            MostWins[match.winner] += 1;
-          } else {
-            MostWins[match.winner] = 1;
+    const chart_5_data = (year) => {
+      let MostWins = {};
+      data.data &&
+        data.data.forEach((match, idx) => {
+          if (match.season === parseInt(year)) {
+            if (MostWins[match.winner] !== undefined) {
+              MostWins[match.winner] += 1;
+            } else {
+              MostWins[match.winner] = 1;
+            }
           }
-        }
+        });
+
+      //For finding the top 5 teams
+      const keys = [...Object.keys(MostWins)];
+      const values = [...Object.values(MostWins)];
+
+      const arrayOfObj = keys.map(function (d, i) {
+        return {
+          name: d,
+          data: values[i] || 0,
+        };
       });
 
-    //For finding the top 5 teams
-    const keys = [...Object.keys(MostWins)];
-    const values = [...Object.values(MostWins)];
+      const sortedArrayOfObj = arrayOfObj.sort(function (a, b) {
+        return b.data - a.data;
+      });
+      const newArrayName = [];
+      const newArrayData = [];
 
-    const arrayOfObj = keys.map(function (d, i) {
-      return {
-        name: d,
-        data: values[i] || 0,
-      };
-    });
+      const filteredObj = sortedArrayOfObj.filter((ele, idx) => {
+        return idx < 5;
+      });
+      filteredObj.forEach(function (d) {
+        newArrayName.push(d.name);
+        newArrayData.push(d.data);
+      });
 
-    const sortedArrayOfObj = arrayOfObj.sort(function (a, b) {
-      return b.data - a.data;
-    });
-    const newArrayName = [];
-    const newArrayData = [];
-
-    const filteredObj = sortedArrayOfObj.filter((ele, idx) => {
-      return idx < 5;
-    });
-    filteredObj.forEach(function (d) {
-      newArrayName.push(d.name);
-      newArrayData.push(d.data);
-    });
-
-    setCharData5({
-      labels: [...newArrayName],
-      datasets: [
-        {
-          label: "Wins",
-          data: [...newArrayData],
-          backgroundColor: [
-            "rgba(255, 159, 64, 0.5)",
-            "rgba(255, 99, 132, 0.5)",
-            "rgba(54, 162, 235, 0.5)",
-            "rgba(245, 135, 31, 0.5)",
-            "rgba(128, 203, 174, 0.5)",
-          ],
-          borderColor: [
-            "rgba(255, 159, 64, 1)",
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(245, 135, 31, 1)",
-            "rgba(128, 203, 174, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    });
-  };
+      setCharData5({
+        labels: [...newArrayName],
+        datasets: [
+          {
+            label: "Wins",
+            data: [...newArrayData],
+            backgroundColor: [
+              "rgba(255, 159, 64, 0.5)",
+              "rgba(255, 99, 132, 0.5)",
+              "rgba(54, 162, 235, 0.5)",
+              "rgba(245, 135, 31, 0.5)",
+              "rgba(128, 203, 174, 0.5)",
+            ],
+            borderColor: [
+              "rgba(255, 159, 64, 1)",
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(245, 135, 31, 1)",
+              "rgba(128, 203, 174, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      });
+    };
+    chart_5_data(year);
+  }, [year, data.data]);
 
   return (
     <>
