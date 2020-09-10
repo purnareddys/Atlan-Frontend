0895aaa747cf27b38de9e913220ee788f496ae7f
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-09-11 02:19:03 +0530

    testing for responsiveness

--- src/components/display-chart/ChartDisplay.js
+++ src/components/display-chart/ChartDisplay.js
@@ -12,66 +12,71 @@ export default function ChartDisplay({ chartType, chartData, ...restProps }) {
     selectLabelText: "",
   };
   return (
-    <div className="chart">
-      <ChartComponent
-        type={chartType}
-        data={chartData}
-        options={{
-          plugins: {
-            deferred: {
-              xOffset: 50,
-              yOffset: "40%",
-              delay: 70,
-            },
-          },
-          maintainAspectRatio: false,
-          responsive: true,
-          title: {
-            display: defaultProps.displayTitle,
-            text: restProps.titleText,
-            fontSize: 25,
-          },
-          legend: {
-            display: restProps.displayLegend,
-          },
-          scales:
-            chartType === "pie" || chartType === "doughnut"
-              ? {
-                  xAxis: [
-                    {
-                      gridLineWidth: 0,
-                    },
-                  ],
-                  yAxis: [
-                    {
-                      gridLineWidth: 0,
-                      minorTickInterval: null,
-                    },
-                  ],
-                }
-              : {
-                  xAxes: [
-                    {
-                      scaleLabel: {
-                        display: false,
-                        labelString: defaultProps.selectLabelText,
-                      },
-                      ticks: {
-                        beginAtZero: true,
-                        max: 10,
-                      },
-                    },
-                  ],
-                  yAxes: [
-                    {
-                      ticks: {
-                        beginAtZero: true,
-                      },
-                    },
-                  ],
-                },
-        }}
-      />
-    </div>
+    // <div className="chart">
+    //   <ChartComponent
+    //     type={chartType}
+    //     data={chartData}
+    //     options={{
+    //       plugins: {
+    //         deferred: {
+    //           xOffset: 50,
+    //           yOffset: "40%",
+    //           delay: 70,
+    //         },
+    //       },
+    //       maintainAspectRatio: false,
+    //       responsive: true,
+    //       title: {
+    //         display: defaultProps.displayTitle,
+    //         text: restProps.titleText,
+    //         fontSize: 25,
+    //       },
+    //       legend: {
+    //         display: restProps.displayLegend,
+    //       },
+    //       scales:
+    //         chartType === "pie" || chartType === "doughnut"
+    //           ? {
+    //               xAxis: [
+    //                 {
+    //                   gridLineWidth: 0,
+    //                 },
+    //               ],
+    //               yAxis: [
+    //                 {
+    //                   gridLineWidth: 0,
+    //                   minorTickInterval: null,
+    //                 },
+    //               ],
+    //             }
+    //           : {
+    //               xAxes: [
+    //                 {
+    //                   scaleLabel: {
+    //                     display: false,
+    //                     labelString: defaultProps.selectLabelText,
+    //                   },
+    //                   ticks: {
+    //                     beginAtZero: true,
+    //                     max: 10,
+    //                   },
+    //                 },
+    //               ],
+    //               yAxes: [
+    //                 {
+    //                   ticks: {
+    //                     beginAtZero: true,
+    //                   },
+    //                 },
+    //               ],
+    //             },
+    //     }}
+    //   />
+    // </div>
+    <>
+      <div class="grid-item">grid item</div>
+      <div class="grid-item">grid item</div>
+      <div class="grid-item">grid item</div>
+    </>
   );
 }
